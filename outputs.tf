c8fe1097d21b9dea56cbb23e596b293156b5c7ea
lsmin3388 <lsmin3388_@naver.com>
2026-06-06 14:45:52 +0900

    feat(infra): add second E2.1.Micro VM for the frontend

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,11 @@ output "instance_id" {
 output "ssh_command" {
   value = "ssh -i ~/.ssh/knup_oci ubuntu@${oci_core_instance.app.public_ip}"
 }
+
+output "frontend_public_ip" {
+  value = oci_core_instance.frontend.public_ip
+}
+
+output "frontend_ssh_command" {
+  value = "ssh -i ~/.ssh/knup_oci ubuntu@${oci_core_instance.frontend.public_ip}"
+}
